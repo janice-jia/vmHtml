20694872f7837f06856adc7b18c819532cc9dfbb
yunwu <dhj63e@163.com>
2016-06-16 14:55:09 +0800

    注册6.16+

---
+++ html/static/js/src/regist.js
@@ -0,0 +1,7 @@
+/**
+ * Created by user001 on 2016/6/16.
+ */
+$("#regist_submit").click(function(){
+    $("[data-regist='registnow']").css("display","none");
+    $('[data-regist="regfinish"]').css("display","block");
+});

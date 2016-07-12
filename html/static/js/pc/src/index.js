5754e2d552c44cfff034e33ca0c455e4963ce222
xiaojia <2434220573@qq.com>
2016-07-12 17:29:25 +0800

    首页调整

--- html/static/js/pc/src/index.js
+++ html/static/js/pc/src/index.js
@@ -1,10 +1,35 @@
-function loadHeaderFooter()
-{
+function loadHeaderFooter() {
     $("#header").load("./header.html");
     $("#footer").load("./footer.html");
     $("#page").load("./_page.html");
     $("#comments").load("./_comments.html");
 }
-
 loadHeaderFooter();
 
+function scroll(obj)
+{
+    var tmp = (obj.scrollLeft)++;
+    //当滚动条到达右边顶端时
+    if (obj.scrollLeft == tmp)
+        obj.innerHTML += obj.innerHTML;
+    //当滚动条滚动了初始内容的宽度时滚动条回到最左端
+    if (obj.scrollLeft >= obj.firstChild.offsetWidth)
+        obj.scrollLeft = 0;
+}
+
+window.onload = function() {
+    var pic = document.getElementById("scrollobj");
+
+    var myRoll = setInterval(function(){
+        scroll(document.getElementById('scrollobj'));
+    }, 30);
+
+    pic.onmouseover = function() { clearInterval(myRoll); };
+    pic.onmouseout = function() {
+        setInterval(
+            myRoll = setInterval(function(){
+                scroll(document.getElementById('scrollobj'));
+            }, 30)
+        );
+    };
+};

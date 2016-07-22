80f231647997284529ab8357b93e335bd562bf93
xiaojia <2434220573@qq.com>
2016-07-22 11:41:44 +0800

    编辑文字作品修改

--- html/static/js/pc/src/personal.js
+++ html/static/js/pc/src/personal.js
@@ -10,64 +10,6 @@ function showHideSet(showHideID){
     }
 }
 
-/*编辑图片start*/
-var spa = 40; //缩略图区域补充数值
-var w = 0;
-
-//设置当前选中
-function setChecked(checkedObj){
-    $('#showArea img').each(function(){
-        $(this).removeClass('hoverMax');
-    });
-    $('#mainphoto').attr('src',checkedObj.attr('rel'));
-    checkedObj.addClass('hoverMax');
-}
-
-function goLeft(){
-    if($(".hoverMax").prevAll().length > 0){
-        setChecked($(".hoverMax").prev('img'));
-        if($(".hoverMax").nextAll().length>4){
-            var marginLeft = parseInt($('#showArea').css('marginLeft'));
-            if(isNaN(marginLeft)){
-                marginLeft = 0;
-            }
-            $('#showArea').css('marginLeft',(marginLeft+170)+'px');
-        }
-    }
-}
-
-function goRight(){
-    if($(".hoverMax").nextAll().length > 0){
-        setChecked($(".hoverMax").next());
-        if($(".hoverMax").prevAll().length>4){
-            var marginLeft = parseInt($('#showArea').css('marginLeft'));
-            if(isNaN(marginLeft)){
-                marginLeft = 0;
-            }
-            $('#showArea').css('marginLeft',(marginLeft-170)+'px');
-        }
-    }
-}
-
-$(function() {
-    var rHtml = '';
-    $('#showArea img').each(function(){
-        w += parseInt($(this).attr('width')) + spa;
-        $(this).click(function() {
-            setChecked($(this));
-        });
-        rHtml += '<img src="' + $(this).attr('rel') + '" width="0" height="0" alt="" />';
-    });
-    $('#showArea').css('width',parseInt(w) + 'px');
-    var rLoad = document.createElement("div");
-    $('#photos').append(rLoad);
-    rLoad.style.width = "1px";
-    rLoad.style.height = "1px";
-    rLoad.style.overflow = "hidden";
-    rLoad.innerHTML = rHtml;
-});
-/*编辑图片end*/
-
 
 //文字作品--编辑
 function showEditText(){

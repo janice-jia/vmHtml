e9766d9c778827dd85d2620b5eb2aeca4fc2be38
yunwu <dhj63e@163.com>
2016-08-05 16:39:20 +0800

    查看漫画

---
+++ html/static/js/pc/version1.1/tribe-vimg.js
@@ -0,0 +1,27 @@
+/**
+ * Created by dhj on 8/5/16.
+ */
+//显示吐槽列表
+$('#tribe-img-aside-top').click(function(){
+    $('#tribe-img-aside-comment').css({
+        right:'0'
+    });
+});
+//判断一个元素是否是另一个元素的子元素
+function isInComment (obj,parentObj){
+    while (obj != undefined && obj != null && obj.tagName.toUpperCase() != 'BODY'){
+        if (obj == parentObj){
+            return true;
+        }
+        obj = obj.parentNode;
+    }
+    return false;
+}
+//判断event是否是吐槽区域内
+$(document).click(function(event){
+    if(! isInComment(event.target, $("#tribe-img-aside-top")[0])){
+        $('#tribe-img-aside-comment').css({
+            right:'-210px'
+        });
+    }
+});

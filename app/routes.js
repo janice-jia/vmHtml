aaac0a5d7dab6e3680f05b6a8f3029c2b93b1400
yunwu <dhj63e@163.com>
2016-11-07 18:32:58 +0800

    tribe-album

--- app/routes.js
+++ app/routes.js
@@ -8,6 +8,7 @@ import Require from './components/Require'
 import RequireInfo from './components/RequireInfo'
 import Tribe from './components/Tribe'
 import TribeInfo from './components/TribeInfo'
+import TribeAlbum from './components/TribeAlbum'
 
 module.exports = (
     <Router history={browserHistory}>
@@ -19,6 +20,7 @@ module.exports = (
             <Route path="/require/info/:requireId" component={RequireInfo}/>
             <Route path="/tribe" component={Tribe}/>
             <Route path="/tribe/info/:tribeId" component={TribeInfo}/>
+            <Route path="/tribe/album/:albumId" component={TribeAlbum}/>
         </Route>
     </Router>
 )

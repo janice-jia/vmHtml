9132c25c0c9e67aae35fc6224f7b4535969962a0
xiaojia <2434220573@qq.com>
2016-11-24 16:15:04 +0800

    edit webpack.server

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,28 +1,34 @@
 var fs = require('fs')
 var path = require('path')
+var htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'server.js'),
-  output: {
-    filename: 'server.bundle.js'
-  },
-  target: 'node',
-  // keep node_module paths out of the bundle
-  externals: fs.readdirSync(path.resolve(__dirname, 'node_modules')).concat([
-    'react-dom/server'
-  ]).reduce(function (ext, mod) {
-    ext[mod] = 'commonjs ' + mod
-    return ext
-  }, {}),
-  node: {
-    __filename: false,
-    __dirname: false
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' },
-      {test: /\.scss$/, loader: "style-loader!css-loader!sass"},
-      {test: /\.woff|\.woff2|\.svg|.eot|\.ttf/,loader: 'url?prefix=font/&limit=10000'}
+    entry: path.resolve(__dirname, 'server.js'),
+    output: {
+        filename: 'server.bundle.js'
+    },
+    target: 'node',
+    // keep node_module paths out of the bundle
+    externals: fs.readdirSync(path.resolve(__dirname, 'node_modules')).concat([
+        'react-dom/server'
+    ]).reduce(function (ext, mod) {
+        ext[mod] = 'commonjs ' + mod
+        return ext
+    }, {}),
+    node: {
+        __filename: false,
+        __dirname: false
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react'},
+            {test: /\.scss$/, loader: "style-loader!css-loader!sass"},
+            {test: /\.woff|\.woff2|\.svg|.eot|\.ttf/, loader: 'url?prefix=font/&limit=10000'}
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            title: 'vmking',
+        }),
     ]
-  }
 }
